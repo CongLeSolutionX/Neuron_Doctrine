0c0a0c36277ded8a81941434c613f0a6a29e55e5
Cong Le <longchike@gmail.com>
2025-06-29 08:21:40 -0700

    The setup

---
+++ Neuron_Doctrine/Neuron_DoctrineApp.swift
@@ -0,0 +1,17 @@
+//
+//  Neuron_DoctrineApp.swift
+//  Neuron_Doctrine
+//
+//  Created by Cong Le on 6/29/25.
+//
+
+import SwiftUI
+
+@main
+struct Neuron_DoctrineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
